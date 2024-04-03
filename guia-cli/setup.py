6eae7d5accb08f18d454ab0e92cc6b182d378670
andersonbosa <andersonbosa0@gmail.com>
2024-04-03 13:27:13 -0300

    style: rename package and entry point names to use underscores

--- guia-cli/setup.py
+++ guia-cli/setup.py
@@ -4,16 +4,16 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="guia-cli",
+    name="guia_cli",
     version="0.1",
     packages=find_packages(),
     include_package_data=True,
     install_requires=requirements,
     entry_points={
         "console_scripts": [
-            "gu = guia-cli.main:main",
-            "guia = guia-cli.main:main",
-            "gucli = guia-cli.main:main",
+            "gu = guia_cli.main:main",
+            "guia = guia_cli.main:main",
+            "gucli = guia_cli.main:main",
         ],
     },
 )
